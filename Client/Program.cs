39052204f1fe83111de832fada7aa79c4a91ce28
Michael McKechney <mimcke@microsoft.com>
2022-08-16 15:27:36 -0400

    any origin

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Microsoft.Extensions.Options;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,9 +11,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 builder.Services.AddCors(options =>
     {
-        options.AddPolicy(name: "any", policy =>
-                          {
-                              policy.WithOrigins("*");
-                          }); ;
+        options.AddPolicy(
+            "Open",
+            builder => builder.AllowAnyOrigin().AllowAnyHeader());
     });
 await builder.Build().RunAsync();
+
+
